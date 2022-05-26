da63e07210c54e7b8779f914493a0b5c16b9a2e1
ArusyakAghajanyan <ayad_aga@mail.ru>
2022-05-26 15:41:06 +0400

    done

---
+++ .history/src/components/ToDoItem_20220526122045.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './toDoItem.css'
+
+
+export default function ToDoItem({item,modalStatus,onChange}) {
+    return (
+        <div className="todoitem">
+           <label id="check"  htmlFor="" 
+           >
+           <input type="checkbox" checked={item.isCompleted} onChange={(e)=> {
+           onChange({
+               ...item,
+               isCompleted:e.target.checked,
+           })
+           }}/>
+            {item.text}
+            
+           </label>  
+           <button className="todoitemDelete" onClick={() => {
+            modalStatus(true,item)
+            }}>x</button>        
+        </div>
+    )
+}
---
+++ .history/src/components/ToDoItem_20220526122535.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './toDoItem.css'
+
+
+export default function ToDoItem({item,modalStatus,onChange}) {
+    return (
+        <div className="todoitem">
+           <label id="check"  htmlFor="" 
+           style={item.isCompleted ? {color:"#ACACAC"}:}
+           >
+           <input type="checkbox" checked={item.isCompleted} onChange={(e)=> {
+           onChange({
+               ...item,
+               isCompleted:e.target.checked,
+           })
+           }}/>
+            {item.text}
+            
+           </label>  
+           <button className="todoitemDelete" onClick={() => {
+            modalStatus(true,item)
+            }}>x</button>        
+        </div>
+    )
+}
